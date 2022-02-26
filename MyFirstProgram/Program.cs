d0e38aed2576d6da20eb2fe15e1465b3e235312a
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-26 14:04:25 +0800

    finish with variables 😎

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,8 +6,37 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            int x = 123;
-            Console.WriteLine(x);
+            int x;
+            x = 123;
+
+            int y = 321;
+
+            int z = x + y;
+
+            int age = 21;
+            double height = 300.5;
+
+            bool alive = true;
+
+            char symbol = '@';
+
+            String name = "Alvin";
+
+            String username = symbol + name;
+
+            Console.WriteLine("Your age is " + age);
+
+            Console.WriteLine("Your height is " + height + "cm");
+
+            Console.WriteLine("Are you alive? " + alive);
+
+            Console.WriteLine("Your symbol is " + symbol);
+
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Your username is " + username);
+
+            Console.ReadKey();
         }
     }
 }
