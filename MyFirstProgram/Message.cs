7641112d50d2699ff2d4d8a3c2611af4ce5de0ec
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-27 01:25:35 +0800

    done working in static classes

---
+++ MyFirstProgram/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    static class Message
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello!. Welcome to the program.");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("I am waiting for someone.");
+        }
+
+        public static void Bye()
+        {
+            Console.WriteLine("Bye! Thanks for visiting.");
+        }
+
+    }
+}
--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,33 +6,9 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
+            Message.Hello();
 
-            String[] ford = { "Mustang", "F-150", "Explorer" };
-            String[] chevy = { "Corvette", "Camaro", "Silverado" };
-            String[] toyota = { "Corrola", "Camry", "Rav4" };
-
-            String[,] parkingLot = {
-                {"Mustang", "F-150", "Explorer" },
-                { "Corvette", "Camaro", "Silverado" },
-                { "Corrola", "Camry", "Rav4" }
-            };
-
-            //parkingLot[0, 2] = "Fusion";
-
-            for(int i = 0; i < parkingLot.GetLength(0); i++)
-            {
-                for(int j = 0; j < parkingLot.GetLength(1); j++ )
-                {
-                    Console.Write(parkingLot[i, j] + " ");
-                }
-                Console.WriteLine("");
-            }
-
-            //foreach(String car in parkingLot)
-            //{
-            //    Console.WriteLine(car);
-            //}
-
+            
             Console.Beep();
             Console.ReadKey();
         }
