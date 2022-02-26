b3229ddefc305819b890bb8bfce51258d63dfaf0
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-26 19:27:16 +0800

    working in switch case

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -7,18 +7,7 @@ namespace MyFirstProgram
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Please enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-
-            if(age >= 18)
-            {
-                Console.WriteLine("You are registered.");
-            }   else
-            {
-                Console.WriteLine("You are not registered.");
-            }
-
-
+            
             Console.Beep();
             Console.ReadKey();
             
