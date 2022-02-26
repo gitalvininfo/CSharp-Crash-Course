0edc4b92a29e125fe304ef771beab5fd7c49a09d
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-27 01:33:33 +0800

    done working with objects instance

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,12 +6,42 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            Message.Hello();
 
-            
+            Human human1 = new Human();
+            human1.name = "Rick";
+            human1.age = 65;
+
+            human1.Eat();
+            human1.Sleep();
+
+            Human human2 = new Human();
+            human2.name = "Morty";
+            human2.age = 60;
+
+            human2.Eat();
+            human2.Sleep();
+
             Console.Beep();
             Console.ReadKey();
         }
+
+    }
+
+    class Human
+    {
+        public String name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine(name + " is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping.");
+        }
+
 
     }
 }
