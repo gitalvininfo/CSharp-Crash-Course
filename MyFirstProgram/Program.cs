fb29bd8dab9c26a78b0de3c43ebad65bb74e7269
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-26 19:48:20 +0800

    done working in logical operators

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,44 +6,19 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What day is it today?");
-            String day = Console.ReadLine();
 
-            switch(day)
-            {
-                case "Monday":
-                    Console.WriteLine("It is Monday");
-                    break;
-
-                case "Tuesday":
-                    Console.WriteLine("It is Tuesday");
-                    break;
-
-                case "Wednesday":
-                    Console.WriteLine("It is Wednesday");
-                    break;
-
-                case "Thursday":
-                    Console.WriteLine("It is Thursday");
-                    break;
-
-                case "Friday":
-                    Console.WriteLine("It is Friday");
-                    break;
-
-                case "Saturday":
-                    Console.WriteLine("It is Saturday");
-                    break;
-
-                case "Sunday":
-                    Console.WriteLine("It is Sunday");
-                    break;
-
-                default:
-                    Console.WriteLine(day + " is not a day!");
-                    break;
-            }
+            Console.WriteLine("What is the temperature outside: (C)");
+            double temp = Convert.ToDouble(Console.ReadLine());
             
+            if(temp >= 10 && temp <=25)
+            {
+                Console.WriteLine("It is warm upside.");
+            }
+            else if (temp <= -50 || temp >= 50)
+            {
+                Console.WriteLine("Do not go outside!");
+            }
+
             Console.Beep();
             Console.ReadKey();
             
