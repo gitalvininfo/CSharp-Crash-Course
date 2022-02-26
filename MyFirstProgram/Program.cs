509af9885552fa480974c7308f053b5bb9cc0ce1
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-26 19:26:18 +0800

    done working in if else 😌

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -7,27 +7,17 @@ namespace MyFirstProgram
         static void Main(string[] args)
         {
 
-            String fullName = "Bro Code";
-            String phoneNumber = "123-465-7890";
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
 
-            //fullName.ToUpper();
-            //fullName.ToLower();
-            //Console.WriteLine(fullName);
+            if(age >= 18)
+            {
+                Console.WriteLine("You are registered.");
+            }   else
+            {
+                Console.WriteLine("You are not registered.");
+            }
 
-            //phoneNumber = phoneNumber.Replace("-", "/");
-            //Console.WriteLine(phoneNumber);
-
-            //String userName = fullName.Insert(0, "@");
-            //Console.WriteLine(userName);
-
-            //Console.WriteLine(fullName.Length);
-
-            // index to start then how many characters to slice;
-            //String firstName = fullName.Substring(0, 3);
-            //Console.WriteLine(firstName);
-
-            String lastName = fullName.Substring(4, 4);
-            Console.WriteLine(lastName);
 
             Console.Beep();
             Console.ReadKey();
