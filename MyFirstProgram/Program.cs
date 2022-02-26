1ccefd0f2f1691cbad791842dbb511047f5d4533
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-27 00:53:16 +0800

    done working in ternary operator

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -7,50 +7,13 @@ namespace MyFirstProgram
         static void Main(string[] args)
         {
 
-            int x, y, result;
-            bool isContinue = true;
+            double temperature = 30;
+            String message;
 
-            while (isContinue)
-            {
-                try
-                {
-                    Console.Write("Enter num 1: ");
-                    x = Convert.ToInt32(Console.ReadLine());
+            message = temperature > 25 ? "Hot" : "Cold";
 
-                    Console.Write("Enter num 2: ");
-                    y = Convert.ToInt32(Console.ReadLine());
-
-                    result = x * y;
-
-                    Console.WriteLine(result);
-                    Console.WriteLine("Do you want to continue? (y/n)");
-
-                    if(Console.ReadLine() == "y")
-                    {
-                        isContinue = true;
-                    }
-                        else
-                    {
-                        isContinue = false;
-                    }
-
-                }
-                catch (FormatException e)
-                {
-                    Console.WriteLine("Enter only numbers pls.");
-                    continue;
-                }
-                catch (DivideByZeroException e)
-                {
-                    Console.WriteLine("You cant divide by zero.");
-                    continue;
-                }
-
-                finally
-                {
-                    Console.WriteLine("Thanks for try catch");
-                }
-            }
+            Console.WriteLine(message);
+            
             Console.Beep();
             Console.ReadKey();
         }
