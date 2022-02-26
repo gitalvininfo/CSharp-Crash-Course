2e400634217cd3df84bc57cfff1919283ce79bf1
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-27 01:15:31 +0800

    done working in 2 dimensional array

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -7,12 +7,31 @@ namespace MyFirstProgram
         static void Main(string[] args)
         {
 
-            String firstname = "Bro";
-            String lastname = "Code";
-            int age = 20;
+            String[] ford = { "Mustang", "F-150", "Explorer" };
+            String[] chevy = { "Corvette", "Camaro", "Silverado" };
+            String[] toyota = { "Corrola", "Camry", "Rav4" };
 
-            Console.WriteLine($"Hello { firstname, 10} { lastname}");
+            String[,] parkingLot = {
+                {"Mustang", "F-150", "Explorer" },
+                { "Corvette", "Camaro", "Silverado" },
+                { "Corrola", "Camry", "Rav4" }
+            };
 
+            //parkingLot[0, 2] = "Fusion";
+
+            for(int i = 0; i < parkingLot.GetLength(0); i++)
+            {
+                for(int j = 0; j < parkingLot.GetLength(1); j++ )
+                {
+                    Console.Write(parkingLot[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+
+            //foreach(String car in parkingLot)
+            //{
+            //    Console.WriteLine(car);
+            //}
 
             Console.Beep();
             Console.ReadKey();
