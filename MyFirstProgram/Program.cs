27b83a1323db1f5866ecbc611bbc7b50a52962d5
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-26 19:35:26 +0800

    done working in switch case

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,7 +6,43 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What day is it today?");
+            String day = Console.ReadLine();
 
+            switch(day)
+            {
+                case "Monday":
+                    Console.WriteLine("It is Monday");
+                    break;
+
+                case "Tuesday":
+                    Console.WriteLine("It is Tuesday");
+                    break;
+
+                case "Wednesday":
+                    Console.WriteLine("It is Wednesday");
+                    break;
+
+                case "Thursday":
+                    Console.WriteLine("It is Thursday");
+                    break;
+
+                case "Friday":
+                    Console.WriteLine("It is Friday");
+                    break;
+
+                case "Saturday":
+                    Console.WriteLine("It is Saturday");
+                    break;
+
+                case "Sunday":
+                    Console.WriteLine("It is Sunday");
+                    break;
+
+                default:
+                    Console.WriteLine(day + " is not a day!");
+                    break;
+            }
             
             Console.Beep();
             Console.ReadKey();
