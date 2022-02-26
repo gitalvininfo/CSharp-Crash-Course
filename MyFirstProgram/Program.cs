64337f4a71cb936445bbe23ead5233eb3782704c
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-26 23:52:51 +0800

    done working in arrays 1d

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,61 +6,22 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            do
+
+            //String[] cars = { "BMW", "Mustang", "Audi", "Lamborghini", "Ferrari" };
+
+            String[] cars = new string[4];
+
+            cars[0] = "BMW";
+            cars[1] = "Mustang";
+            cars[2] = "Audi";
+            cars[3] = "Lamborghini";
+
+
+            for (int i = 0; i < cars.Length; i++)
             {
-                Console.WriteLine("===================");
-                Console.WriteLine("Calculator Program");
-                Console.WriteLine("===================");
+                Console.WriteLine("\t" + cars[i]);
+            }
 
-                double num1, num2, result = 0;
-
-                Console.Write("Enter number 1: ");
-                num1 = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Enter number 2: ");
-                num2 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Enter number option: ");
-                Console.WriteLine("\t+ : Add");
-                Console.WriteLine("\t- : Subtract");
-                Console.WriteLine("\t* : Multiply");
-                Console.WriteLine("\t/ : Divide");
-
-                switch (Console.ReadLine())
-                {
-                    case "+":
-                        result = num1 + num2;
-                        Console.WriteLine("Result is: " + result);
-
-                        break;
-
-                    case "-":
-                        result = num1 - num2;
-                        Console.WriteLine("Result is: " + result);
-
-                        break;
-
-                    case "*":
-                        result = num1 * num2;
-                        Console.WriteLine("Result is: " + result);
-
-                        break;
-
-                    case "/":
-                        result = num1 / num2;
-                        Console.WriteLine("Result is: " + result);
-
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid option...");
-                        break;
-                }
-                Console.WriteLine("Would you like to continue (Y/N): ");
-            } 
-            while (Console.ReadLine().ToUpper() == "Y");
-
-            Console.WriteLine("Bye");
             Console.Beep();
             Console.ReadKey();
             
