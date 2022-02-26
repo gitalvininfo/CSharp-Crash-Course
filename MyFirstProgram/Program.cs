f1d1f954b11b0e9053f98dee2f66518b298fe6ed
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-26 20:25:48 +0800

    done working with nested loop

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,17 +6,27 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            //for(int i = 0; i < 10; i++)
-            //{
-            //    Console.WriteLine(i);
-            //}
 
-            for(int i = 10; i > 0; i--)
+            Console.Write("Enter number of rows: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number of columns: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter symbol: ");
+            String symbol = Console.ReadLine();
+
+
+            for(int i = 0; i < rows; i++)
             {
-                Console.WriteLine(i);
+                for(int j = 0; j < columns; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.Write("\n");
             }
 
-            Console.WriteLine("Happy New Year!!!");
+
             Console.Beep();
             Console.ReadKey();
             
